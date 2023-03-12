77a849253f614f9ba8edc76390f6135d53bb46f1
andreas7500 <andreas2875@gmail.com>
2023-03-12 12:54:18 +0200

    end collr and timer commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let timeId = null;
 
+stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled', false);
+
+  timeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeId);
+  startBtn.removeAttribute('disabled', false);
+  stopBtn.setAttribute('disabled', true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
